eddbf0da811726f0d98534c4bc2efe22b6fad7c3
jbrucker <jebrucker@gmail.com>
2022-01-31 11:43:18 +0700

    show ok output in Github Action

--- runtests.sh
+++ runtests.sh
@@ -5,5 +5,8 @@ if [ ! -f hog.py ]; then
 	echo "No file hog.py"
 	exit 1
 fi
+python3 ok --local
+
+# Run test again and count failing tests
 errors=`python3 ok --local | grep Error | wc -l`
 exit $errors
